88bdac2df58466e02f8ceaa0c9c897d77dc1f545
Tomasz Kula <tomaszkula07@gmail.com>
2015-11-29 00:08:08 +0100

    first animations

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,12 @@
+var cardP = $('<div class=\'cardsPlayer\'>A<p>SUIT</p></div>');
+var cardD = $('<div class=\'cardsDealer\'>A<p>SUIT</p></div>');
+
 $(document).ready(function(){
 	$('#deal').on('click', function(){
-		$(this).closest('#table').find('cardsTop')
+		$(this).closest('#table').find('#cardsbot').append(cardP);
+		$(this).closest('#table').find('#cardstop').append(cardD);
+		$(this).addClass('hidden');
+		$(this).closest('#table').find('#pass').fadeIn();
+		$(this).closest('#table').find('#hit').fadeIn();
 	});
-
 });
