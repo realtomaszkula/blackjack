bf92f6637cc9897bd9eb4af973d4c27494840cf3
Tomasz Kula <tomaszkula07@gmail.com>
2015-11-29 08:40:28 +0100

    added footer

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,19 @@
-var cardP = $('<div class=\'cardsPlayer\'>A<p>SUIT</p></div>');
-var cardD = $('<div class=\'cardsDealer\'>A<p>SUIT</p></div>');
-
 $(document).ready(function(){
 	$('#deal').on('click', function(){
+		var cardP = $('<div class=\'cardsPlayer\'>A<p>SUIT</p></div>');
+		var cardD = $('<div class=\'cardsDealer\'>A<p>SUIT</p></div>');
 		$(this).closest('#table').find('#cardsbot').append(cardP);
 		$(this).closest('#table').find('#cardstop').append(cardD);
 		$(this).addClass('hidden');
 		$(this).closest('#table').find('#pass').fadeIn();
 		$(this).closest('#table').find('#hit').fadeIn();
 	});
+
+	$('#hit').on('click', function(){
+		var newCard = $('<div class=\'cardsPlayer\'>A<p>SUIT</p></div>');
+		$(this).closest('#table').find('#cardsbot').append(newCard);
+	});
+
 });
+
+
