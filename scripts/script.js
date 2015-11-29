4d71b0c16ff289f883d6d257db0ff7c89c248aaa
Tomasz Kula <tomaszkula07@gmail.com>
2015-11-29 10:01:38 +0100

    added deck, getCard

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
 	$('#deal').on('click', function(){
-		var cardP = $('<div class=\'cardsPlayer\'>A<p>SUIT</p></div>');
-		var cardD = $('<div class=\'cardsDealer\'>A<p>SUIT</p></div>');
+		deal();
+		var D = getCard();
+		var P = getCard();
+		var cardP = $('<div class=\'cardsPlayer\'>'+P.value+'<p>'+P.suit+'</p></div>');
+		var cardD = $('<div class=\'cardsDealer\'>'+D.value+'<p>'+D.suit+'</p></div>');
 		$(this).closest('#table').find('#cardsbot').append(cardP);
 		$(this).closest('#table').find('#cardstop').append(cardD);
 		$(this).addClass('hidden');
@@ -10,10 +13,50 @@ $(document).ready(function(){
 	});
 
 	$('#hit').on('click', function(){
-		var newCard = $('<div class=\'cardsPlayer\'>A<p>SUIT</p></div>');
+		var P = getCard();
+		var newCard = $('<div class=\'cardsPlayer\'>'+P.value+'<p>'+P.suit+'</p></div>');
 		$(this).closest('#table').find('#cardsbot').append(newCard);
 	});
 
 });
 
+var Card = function(value, suit, isDrawn){
+	this.value = value;
+	this.suit = suit;
+	this.isDrawn = isDrawn;
+}
 
+var deck = [];
+var deal = function () {
+	var k = 0;
+	for (var i = 2; i<15; i++, k++)
+		deck[k] = new Card(i,'spade',false);
+	for (var i = 2; i<15; i++, k++)
+		deck[k] = new Card(i,'heart',false);
+	for (var i = 2; i<15; i++, k++)
+		deck[k] = new Card(i,'diamond',false);
+	for (var i = 2; i<15; i++, k++)
+		deck[k] = new Card(i,'club',false);
+
+	for(var i = 0; i < deck.length; i++){
+		var x = deck[i];
+		if(x.value == 10) x.value ='T';
+		if(x.value == 11) x.value ='J';
+		if(x.value == 12) x.value ='Q';
+		if(x.value == 13) x.value ='K';
+		if(x.value == 14) x.value ='A';
+	}
+};
+
+var getCard = function(){
+	var x = Math.floor(Math.random() * 52);
+	for(;;){
+		var z = deck[x];
+		if (z.isDrawn == false){
+			z.isDrawn = true; return deck[x];
+		}
+		else {
+			x++; if(x==51) {x = 0;}
+		}
+	}
+};
